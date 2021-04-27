e6af10c80aa48bb69cb0e100362cd2c0b585e49a
Santiago <santiagobastidasrodriguez@gmail.com>
2021-04-27 12:29:26 -0500

    thrusty and falsy class

---
+++ BasesJS/Thrusty-falsy.js
@@ -0,0 +1,43 @@
+Boolean()
+//false
+
+Boolean(0)
+//false
+
+Boolean(1)
+//true
+
+Boolean(null)
+//false
+
+Boolean(NaN) /* Not a Number */
+//false
+
+Boolean(undefined)
+//false
+
+Boolean(false)
+//false
+
+Boolean('')
+//false
+
+
+Boolean('a')
+//true
+
+Boolean(1)
+//true
+
+Boolean([]) /* Array */
+//true
+
+Boolean({}) /* Object */
+//true
+
+Boolean(function(){})
+//true
+
+Boolean(true)
+//true
+
