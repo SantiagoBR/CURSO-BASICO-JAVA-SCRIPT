37d6c3cc75209a788cb121b725257244cdb77c1f
Santiago <santiagobastidasrodriguez@gmail.com>
2021-04-27 12:28:44 -0500

    Scope class

---
+++ BasesJS/Scope.js
@@ -0,0 +1,11 @@
+var miNombre = "Santiago";
+
+function nombre() 
+{
+    var miApellido = "Bastidas";
+    console.log(miNombre + " " + miApellido);
+}
+
+nombre();
+
+//El resultado debe ser: Santiago Bastidas
