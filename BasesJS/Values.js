62c774754665773f90dc592571b2e5f60cd391c5
Santiago <santiagobastidasrodriguez@gmail.com>
2021-04-27 12:30:39 -0500

    Values class

---
+++ BasesJS/Values.js
@@ -0,0 +1,22 @@
+//Valores númericos
+40
+
+//Valores de texto
+'Santiago Bastidas' 
+
+
+//Valores booleanos
+true 
+false
+
+
+//Valores vacío - Empty values
+null 
+undefined 
+
+//Valores array
+[1,2,3]
+
+//Valores tipo objeto
+
+{nombre: "Santiago"}
