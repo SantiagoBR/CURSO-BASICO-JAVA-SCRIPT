bdafe2e85a666a9c5b24cede1bc3365b5a1df308
Santiago <santiagobastidasrodriguez@gmail.com>
2021-04-27 12:28:29 -0500

    Funtions class

---
+++ BasesJS/Functions.js
@@ -0,0 +1,18 @@
+//Declarativas
+
+function miFuncion() 
+{ 
+    return 3;
+}
+
+miFuncion();
+
+//Expresión: Tambiém llamadas funciones anónimas, porque a la función no se le 
+//pone directamente un nombre
+
+var miFucnion = function(a,b)
+{
+    return a + b;
+}
+
+miFuncion(3,1);
