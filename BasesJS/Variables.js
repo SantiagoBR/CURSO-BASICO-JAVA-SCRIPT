463ac23c6cd54f3bdddb2d6dbb4e58f51994ff8a
Santiago <santiagobastidasrodriguez@gmail.com>
2021-04-27 12:28:10 -0500

    Variables class

---
+++ BasesJS/Variables.js
@@ -0,0 +1,15 @@
+var nombre = 'Santiago';
+
+// declarar
+var edad;
+
+// inicializar
+edad = 30;
+
+var elementos = ["Computadora" , "Celular"];
+
+var persona =
+{
+    nombre: 'Santiago',
+    edad: 18
+}
